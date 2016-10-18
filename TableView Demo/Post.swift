923a3057bf7374a19037421aa070fdd8e21a896a
dilpreethcs <dilpreet.singh@hotcocoasoftware.com>
2016-10-18 17:47:54 +0530

    Constructor added to initialise posts.

--- TableView Demo/Post.swift
+++ TableView Demo/Post.swift
@@ -9,9 +9,23 @@
 import Foundation
 
 struct Post {
-    var created_at: String = ""
+    var created_at: String!
     var text: String?
     var user: [String : AnyObject]?
-    var username: String = ""
-    var avatar_image: String = ""
+    var username: String!
+    var avatar_image: String?
+    
+    init(post: [String : AnyObject]) {
+        self.created_at = post["created_at"] as? String
+        
+        // Format time string
+        self.created_at = self.created_at?.stringByReplacingOccurrencesOfString("T", withString: " ")
+        self.created_at = self.created_at?.stringByReplacingOccurrencesOfString("Z", withString: "")
+
+        self.text = post["text"] as? String
+        self.user = post["user"] as? [String : AnyObject]
+        self.username = (post["user"])?["username"] as? String
+        self.avatar_image = ((post["user"])?["avatar_image"])?["url"] as? String
+    }
+    
 }
